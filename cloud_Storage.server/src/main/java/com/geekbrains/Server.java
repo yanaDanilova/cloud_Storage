dad40263eead41cc60e50081af0e410fb2091542
yanad <yana.danilova1509@gmail.com>
2021-11-04 12:01:34 +0100

    homework1

--- cloud_Storage.server/src/main/java/com/geekbrains/Server.java
+++ cloud_Storage.server/src/main/java/com/geekbrains/Server.java
@@ -1,5 +1,24 @@
 package com.geekbrains;
 
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) {
+        try(ServerSocket serverSocket = new ServerSocket(8189)){
+            System.out.println("server started..");
+            while (true){
+                Socket socket = serverSocket.accept();
+                new Thread(new ClientHandler(socket)).start();
+            }
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 
 }
