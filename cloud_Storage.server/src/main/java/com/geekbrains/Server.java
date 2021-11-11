6d18a1483a19080f608b290cf6c0573c2419a348
yanad <yana.danilova1509@gmail.com>
2021-11-11 16:24:40 +0100

    nettyHomework 1

--- cloud_Storage.server/src/main/java/com/geekbrains/Server.java
+++ cloud_Storage.server/src/main/java/com/geekbrains/Server.java
@@ -1,24 +1,45 @@
 package com.geekbrains;
 
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+;
 
 public class Server {
     public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(8189)){
-            System.out.println("server started..");
-            while (true){
-                Socket socket = serverSocket.accept();
-                new Thread(new ClientHandler(socket)).start();
-            }
-
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        new Server().run();
     }
 
+    public void run(){
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workedGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap()
+                    .group(bossGroup,workedGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel channel) throws Exception {
+                            channel.pipeline().addLast(
+                                    new ClientHandler()
+                            );
+                        }
+                    });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8189).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workedGroup.shutdownGracefully();
+        }
+    }
 
 }
